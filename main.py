09e16a497bd8cf9d46376cd88f54b1a76c66ccb0
Limo123123 <90343619+Limo123123@users.noreply.github.com>
2021-09-11 22:02:49 +0200

    Aktualisiere pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -6,32 +6,16 @@ input.onPinPressed(TouchPin.P0, function () {
     }
 })
 input.onGesture(Gesture.FreeFall, function () {
-    if (input.runningTime() == 0) {
-        basic.showNumber(0)
-    } else if (input.runningTime() == 1000) {
-        basic.showNumber(1)
-    } else if (input.runningTime() == 2000) {
-        basic.showNumber(2)
-    } else if (input.runningTime() == 3000) {
-        basic.showNumber(3)
-    } else if (input.runningTime() == 4000) {
-        basic.showNumber(4)
-    } else if (input.runningTime() == 5000) {
-        basic.showNumber(5)
-    } else if (input.runningTime() == 6000) {
-        basic.showNumber(6)
-    } else if (input.runningTime() == 7000) {
-        basic.showNumber(7)
-    } else if (input.runningTime() == 8000) {
-        basic.showNumber(8)
-    } else if (input.runningTime() == 9000) {
-        basic.showNumber(9)
-    } else if (input.runningTime() == 10000) {
-        basic.showNumber(10)
-    }
-})
-input.onPinPressed(TouchPin.P3, function () {
-    basic.showNumber(input.compassHeading())
+    music.playMelody("B A B A B A B A ", 40)
+    music.playMelody("B A B A B A B A ", 80)
+    music.playMelody("B A B A B A B A ", 120)
+    music.playMelody("B A B A B A B A ", 260)
+    music.playMelody("B A B A B A B A ", 300)
+    music.playMelody("B A B A B A B A ", 340)
+    music.playMelody("B A B A B A B A ", 380)
+    music.playMelody("B A B A B A B A ", 420)
+    music.playMelody("B A B A B A B A ", 460)
+    music.playMelody("B A B A B A B A ", 500)
 })
 input.onButtonPressed(Button.A, function () {
     oled96.initDisplay()
